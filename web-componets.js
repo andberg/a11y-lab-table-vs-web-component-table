66de6f3b07dff896dd22845abfa189c1ea4e502a
Andrea Berglund <andreaberglund@outlook.com>
2022-10-26 10:37:48 +0200

    Lade till tabell med div i sig för test och div med display: contents

--- web-componets.js
+++ web-componets.js
@@ -1,39 +1,51 @@
 class WCTable extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    const slot = document.createElement('slot');
+    const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement("slot");
     shadow.appendChild(slot);
   }
 }
-customElements.define('wc-table', WCTable);
+customElements.define("wc-table", WCTable);
+
+class WCTableBody extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement("slot");
+    shadow.appendChild(slot);
+  }
+}
+customElements.define("wc-body", WCTableBody);
 
 class WCRow extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    const slot = document.createElement('slot');
+
+    const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement("slot");
+
     shadow.appendChild(slot);
   }
 }
-customElements.define('wc-tr', WCRow);
+customElements.define("wc-tr", WCRow);
 
 class WCTableHeader extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    const slot = document.createElement('slot');
+    const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement("slot");
     shadow.appendChild(slot);
   }
 }
-customElements.define('wc-th', WCRow);
+customElements.define("wc-th", WCRow);
 
 class WCTableDataCell extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    const slot = document.createElement('slot');
+    const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement("slot");
     shadow.appendChild(slot);
   }
 }
-customElements.define('wc-td', WCTableDataCell);
+customElements.define("wc-td", WCTableDataCell);
